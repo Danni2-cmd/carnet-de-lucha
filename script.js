44c5c015e70ccbf3ababf71bedfb15abf25994cd
Danni2-cmd <dannisalamamca@gmail.com>
2025-10-12 13:08:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function displayCarnet(data, showDownloadButton) {
     if (document.getElementById("qr-code-container")) {
         const qrCodeContainer = document.getElementById("qr-code-container");
         const verificationUrl = `${window.location.origin}${window.location.pathname}?id=${data.numeroDocumento}`;
-        new QRCode(qrCodeContainer, { text: verificationUrl, width: 60, height: 60 }); // QR aún más pequeño
+        new QRCode(qrCodeContainer, { text: verificationUrl, width: 60, height: 60 }); // QR MÁS PEQUEÑO
     }
 
     if (showDownloadButton) {
